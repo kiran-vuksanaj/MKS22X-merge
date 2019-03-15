b8af9a9de68d29c178acac2c1bed150f163ec8ee
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-15 09:52:23 -0400

    class structure, wrapper method for mergesort

---
+++ Merge.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class Merge{
+  public static void mergesort(int[] data){
+    int[] copy = Arrays.copyOfRange(data,0,data.length);
+    msort(data,copy,0,data.length);
+  }
+  private static void msort(int[] data,int[] temp,int lo,int hi){
+
+  }
+}
