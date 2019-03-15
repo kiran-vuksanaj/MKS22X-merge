7720da3ab5c516f138505c476ca9980eb0c749dc
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-15 10:01:14 -0400

    recursive cases commented psuedocode

--- Merge.java
+++ Merge.java
@@ -5,6 +5,16 @@ public class Merge{
     msort(data,copy,0,data.length);
   }
   private static void msort(int[] data,int[] temp,int lo,int hi){
+    if(lo < hi){
+      //recursive case: base case is empty, but implied is do nothing when lo>=hi
+      //find approximate middle index
 
+      //mergesort first half(before middle index)
+
+      //mergesort second half(including and after middle index)
+
+      //merge the two together(the real work)
+
+    }
   }
 }
