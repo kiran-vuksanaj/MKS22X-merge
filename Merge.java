a884748f9f2cbc162981191496c51df243c29f82
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-15 10:06:08 -0400

    details of msort, header for merge

--- Merge.java
+++ Merge.java
@@ -8,13 +8,16 @@ public class Merge{
     if(lo < hi){
       //recursive case: base case is empty, but implied is do nothing when lo>=hi
       //find approximate middle index
-
+      int mid = (lo+hi) / 2;
       //mergesort first half(before middle index)
-
+      msort(temp,data,lo,mid);
       //mergesort second half(including and after middle index)
-
+      msort(temp,data,mid,hi);
       //merge the two together(the real work)
-
+      merge(data,temp,lo,mid,hi);
     }
+  }
+  private static void merge(int[] into,int[] withSorted,int lo,int mid,int hi){
+    
   }
 }
